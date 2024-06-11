b4d9f5f09559072937e3fff2977b9a69ccf5c272
Leandro <ocel99@gmail.com>
2024-06-10 23:44:06 -0300

    Parsing01

---
+++ NetBeansProjects/Parsing01/src/Parsing01.java
@@ -0,0 +1,22 @@
+public class Parsing01 {
+    public static void main(String[] args) {
+        //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
+     String shirtPrice, taxRate, gibberish;
+     int intshirtPrice = Integer.parseInt("15");
+          
+        
+        
+        //Parse shirtPrice and taxRate, and print the total tax
+    
+    double doubletaxRate = Double.parseDouble("0.05");
+        System.out.println(intshirtPrice * doubletaxRate);
+        
+        
+        //Try to parse taxRate as an int
+    //int inttaxRate = Integer.parseInt;    
+        
+        //Try to parse gibberish as an int
+        
+    }
+    
+}
