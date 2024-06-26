682fca8e75c5f81c6a5ca91c7ca8808f21f27606
Leandro <ocel99@gmail.com>
2024-06-26 00:14:40 -0300

    Aula6

---
+++ NetBeansProjects/Aula06/src/main/java/Aula6_1/InfiniteLoop.java
@@ -0,0 +1,16 @@
+package Aula6_1;
+
+public class InfiniteLoop {
+    public static void main(String[] args) {
+   
+    // The for loop will run infinite times.
+    for(int i = 1; i <=5; i++) 
+        System.out.println(  "Olá!!!");
+        
+    
+     
+    // To terminate this program press ctrl + c in the console.
+  }
+} 
+    
+
